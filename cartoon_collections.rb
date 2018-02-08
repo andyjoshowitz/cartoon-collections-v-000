1e3acb8f6a43857507ee32ccbce0c7d51420eb3a
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-08 15:47:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(short_words)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.detect do |snacks|
+    
 end
