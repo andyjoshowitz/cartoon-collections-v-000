741353c2495af41c5c23f7bea0807d75af259f9b
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-08 15:45:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,10 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  short_words.any? do |words|
+    words.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
